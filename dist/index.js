43c0b57c6713d34e91a33f3441e77eb8e2a26778
“rezan5910” <bytemefirst@gmail.com>
2024-04-13 09:49:35 +0100

    🚀 functioning cli app pre npm launch

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,35 @@
 #!/usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const hello = (message) => { };
-console.log('wow');
+const commander_1 = require("commander");
+//  declare the program 
+const program = new commander_1.Command();
+// add actions to the program 
+program
+    .action(() => {
+    console.log('Hello World!!');
+})
+    .description('A simple hello world program');
+//  this program capitalises the string on -c
+program
+    .argument("<string>", "string to log")
+    .option("-c, capitalise", "Capitalise the message")
+    .action((message, opts) => {
+    if (opts.capitalise) {
+        console.log(message.toUpperCase());
+    }
+    else {
+        console.log(message);
+    }
+}
+// execute cli witht he given arguments
+)
+    .description('Say Hello');
+program
+    .command("add <numbers>")
+    .action((numbers) => {
+    const total = numbers.reduce((a, b) => a + b, 0);
+    console.log(`Total: ${total}`);
+})
+    .description("Add numbers and log the total");
+program.parse(process.argv);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,48 @@
 #!/usr/bin/env node
-const hello = (message: string) => {}
-console.log('wow')
+import fs from 'fs';
+import { Command } from 'commander';
+
+//  declare the program 
+const program = new Command();
+
+// add actions to the program 
+program
+    .action(() => {
+        console.log('Hello World!!');
+    })
+    .description('A simple hello world program');
+
+
+    //  this program capitalises the string on -c
+program
+    .argument("<string>", "string to log")
+    .option("-c, capitalise", "Capitalise the message")
+    .action(
+        (
+            message: string,
+            opts: {
+                capitalise?: boolean;
+            },
+        ) => {
+            if (opts.capitalise) {
+                console.log(message.toUpperCase());
+
+            } else {
+                console.log(message)
+            }
+        }
+        // execute cli witht he given arguments
+    )
+    .description('Say Hello')
+
+    program
+        .command("add <numbers>")
+        .action((numbers:number[])=>{
+            const total = numbers.reduce((a,b)=> a + b, 0);
+            console.log(`Total: ${total}`)
+        })
+        .description("Add numbers and log the total")
+
+
+
+program.parse(process.argv);
